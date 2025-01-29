75a7d5c165dcf4e38a76befbd42895b1abb36580
Douglas Groene <dgroene@Doug-personal-MacBook.local>
2025-01-29 00:28:25 -0500

    uses generator in  recursive look ahead function

--- src/Moves/SmarterSurvivalMoveManager.php
+++ src/Moves/SmarterSurvivalMoveManager.php
@@ -12,22 +12,40 @@ class SmarterSurvivalMoveManager extends BaseMoveManager {
     public function getMoves(string|null $snakeId = '', $allMoves = self::ALLMOVES): array {
         $impossibleMoveManager = new ImpossibleMoveManager($this->gameData);
         $possibleMoves = $impossibleMoveManager->getMoves();
-        $moveLookAheads = [];
-        foreach ($possibleMoves as $move) {
-            $newGameData = $this->gameData->getNextMoveGameData($move);
-            if ($newGameData === NULL) {
-                $moveLookAheads[$move] = new LookAhead(NULL, 0);
-                continue;
-            }
-            $moveLookAheads[$move] = $this->getDeepestLookAhead($newGameData, self::MAX_DEPTH);
+        if (count($possibleMoves) == 0 || count($possibleMoves) == 1) {
+            return $possibleMoves;
         }
-        return [$this->getBestMove($moveLookAheads, TRUE)];
+        return [$this->compareMoves($possibleMoves)];
     }
 
-    public function getDeepestLookAhead(GameData $gameData, int $stepsRemaining): LookAhead {
+    function compareMoves($moves): string {
+       $bestMove = [];
+       $bestScore = 0;
+       foreach ($moves as $move) {
+           $nextState = $this->gameData->getNextMoveGameData($move);
+           if ($nextState === NULL) {
+             continue;
+           }
+           foreach($this->getDeepestLookAhead($nextState, self::MAX_DEPTH) as $lookAhead) {
+               $score = $this->scoreLookAhead($lookAhead, TRUE);
+               print("Move: $move, Score: $score\n");
+               if ($score > $bestScore) {
+                   $bestMove = [$move];
+                   $bestScore = $score;
+               }
+               else if ($score == $bestScore) {
+                   $bestMove[] = $move;
+               }
+           }
+       }
+       return $bestMove[array_rand($bestMove)];
+    }
+
+    public function getDeepestLookAhead(GameData $gameData, int $stepsRemaining) {
         // Base case: If we have no more steps to survive, we've succeeded.
         if ($stepsRemaining <= 0) {
-            return new LookAhead($gameData, self::MAX_DEPTH);
+            yield new LookAhead($gameData, self::MAX_DEPTH);
+            return;
         }
 
         // Instantiate a temporary manager to explore this state
@@ -36,7 +54,8 @@ class SmarterSurvivalMoveManager extends BaseMoveManager {
 
         // If no moves are possible, we can't survive
         if (empty($nextMoves)) {
-            return new LookAhead($gameData, self::MAX_DEPTH - $stepsRemaining);
+            yield new LookAhead($gameData, self::MAX_DEPTH - $stepsRemaining);
+            return;
         }
 
         // Try each possible move
@@ -45,21 +64,96 @@ class SmarterSurvivalMoveManager extends BaseMoveManager {
             $nextState = $gameData->getNextMoveGameData($nextMove);
 
             if ($nextState === NULL) {
-                $thisPath = new LookAhead($gameData, self::MAX_DEPTH - $stepsRemaining);
+                continue;
             }
-            else $thisPath = $this->getDeepestLookAhead($nextState, $stepsRemaining - 1);
-
-            if ($thisPath->depth > $bestMove->depth) {
-                $bestMove = $thisPath;
-            }
-            else if ($thisPath->depth == $bestMove->depth) {
-                $tiebreak = $this->getBestMove(['existing' => $bestMove, $nextMove => $thisPath]);
-                if ($tiebreak == $nextMove) {
-                    $bestMove = $thisPath;
+            else {
+                foreach ($this->getDeepestLookAhead($nextState, $stepsRemaining - 1) as $lookAhead) {
+                    if ($lookAhead->depth > $bestMove->depth) {
+                        $bestMove = $lookAhead;
+                    }
+                    else if ($lookAhead->depth == $bestMove->depth) {
+                        $bestMoveScore = $this->scoreLookAhead($bestMove);
+                        $thisPathScore = $this->scoreLookAhead($lookAhead);
+                        if ($thisPathScore > $bestMoveScore) {
+                            $bestMove = $lookAhead;
+                        }
+                    }
                 }
             }
         }
-        return $bestMove;
+        yield $bestMove;
+    }
+
+    public function scoreLookAhead(LookAhead $lookAhead, bool $useAccessibleSquares = FALSE): int {
+        $score = 0;
+        $depth = $lookAhead->depth;
+        $new_length = $lookAhead->gameData->getYouLength();
+        $new_health = $lookAhead->gameData->getYou()['health'];
+        $new_snakeCount = $lookAhead->gameData->getSnakeCount();
+
+        $current_length = $this->gameData->getYouLength();
+        $current_health = $this->gameData->getYou()['health'];
+        $current_snakeCount = $this->gameData->getSnakeCount();
+        if ($this->checkPrimedForKilling($lookAhead)) {
+            $score += 12;
+            if ($useAccessibleSquares) {
+                print("Primed for killing+5\n");
+            }
+        }
+        $score += $depth + 2;
+        if ($useAccessibleSquares) {
+            print("$depth + 2\n");
+        }
+        if ($new_length > $current_length) {
+            $score += 10;
+            if ($useAccessibleSquares) {
+                print("length +10 \n");
+            }
+        }
+        if ($new_health > $current_health) {
+            $score += 8;
+            if ($useAccessibleSquares) {
+                print("health +8 \n");
+            }
+        }
+        if ($new_snakeCount < $current_snakeCount) {
+            $score += 7;
+            if ($useAccessibleSquares) {
+                print("deadsnakes +7 \n");
+            }
+        }
+        if ($useAccessibleSquares) {
+            $new_accessibleSquares = $lookAhead->gameData->calculateAccessibleSquares($lookAhead->gameData->getYouHead(), $lookAhead->gameData->getYou()['id']);
+            $current_accessibleSquares = $this->gameData->calculateAccessibleSquares($this->gameData->getYouHead(), $this->gameData->getYou()['id']);
+            if ($new_accessibleSquares > $current_accessibleSquares) {
+                $score += 7;
+                print("accessible squares +7\n");
+            }
+            $closest_snake = [];
+            $closest_distance = 1000;
+            foreach ($this->gameData->getSnakes() as $snake) {
+                if ($snake['id'] == $this->gameData->getYou()['id']) {
+                    continue;
+                }
+                $distance = $this->getManhattanDistance($this->gameData->getYouHead(), $snake['head']);
+                if ($distance < $closest_distance) {
+                    $closest_distance = $distance;
+                    $closest_snake = $snake;
+                }
+            }
+            if (!empty($closest_snake) && !empty($lookAhead->gameData->getSnakeById($closest_snake['id']))) {
+                $currentEnemyAs = $this->gameData->calculateAccessibleSquares($this->gameData->getSnakeHead($closest_snake['id']), $closest_snake['id']);
+                $newEnemyAs = $lookAhead->gameData->calculateAccessibleSquares($lookAhead->gameData->getSnakeHead($closest_snake['id']), $closest_snake['id']);
+                if ($newEnemyAs < $currentEnemyAs) {
+                    $score += 7;
+                    print ("enemy accessible squares +7\n");
+                }
+            }
+        }
+        if ($useAccessibleSquares) {
+            print("-------------------- \n");
+        }
+        return $score;
     }
 
     public function getBestMove(array $lookAheadsToTest, bool $useAccessibleSquares = FALSE): string {
